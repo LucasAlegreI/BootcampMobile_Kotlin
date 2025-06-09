c6c9f1ff17ba3ebe84bc6d5b759363f69e142d7c
LucasAlegreI <alegreirunlucas@gmail.com>
2025-06-09 17:16:45 -0300

    Ejercicion 1 Final Commit

--- Ejercicio 1/src/Main.kt
+++ Ejercicio 1/src/Main.kt
@@ -1,14 +1,52 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import kotlin.random.Random
+var playing = true
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    val torosYVacasGame = TorosYVacasGame()
+    while (playing){
+        print("Ingrese un numero de 4 digitos: ")
+        val input = readln()
+        if (input.length>4)
+        {
+            println ("Invalido, vuelve a intentarlo")
+            continue
+        }
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+        val (toros,vacas)= torosYVacasGame.compareNumbers(input)
+        println("$toros Toros y  $vacas Vacas")
+        if (toros == 4 ) {
+            print ("!Felicidades! el numero secreto era: ${torosYVacasGame.secretAsString()}")
+            return
+        }
+    }
+}
+class TorosYVacasGame () {
+    val numbers = mutableListOf<Int>()
+
+    init {
+        for (i in 1..4) {
+            var newNumber = 0
+            do {
+                newNumber = Random.nextInt(if (i == 1) 1 else 0, 10)
+            }while (numbers.contains(newNumber))
+            numbers.add(newNumber)
+        }
+    }
+    fun secretAsString(): String = numbers.joinToString(" | ")
+
+    fun compareNumbers(newNumbers: String): Pair<Int,Int>{
+        var toros = 0
+        var vacas = 0
+        for ((i,c) in newNumbers.withIndex()) {
+            if (c.isDigit()) {
+                if (c.digitToInt() == numbers[i]) {
+                    toros++
+                } else if (numbers.contains(c.digitToInt())) {
+                    vacas++
+                }
+            }
+        }
+        return toros to vacas
     }
 }
